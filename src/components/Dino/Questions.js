503989a6f9931800d438f5b8bae170e626ef1d2b
Jagdev Panda <jagdevpanda@Jagdevs-MacBook-Air.local>
2024-12-02 22:57:09 +0530

    first commit

---
+++ src/components/Dino/Questions.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+      question: "What is 2 + 2?",
+      options: ["3", "4", "5", "6"],
+      correctAnswer: "4",
+    },
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Rome"],
+      correctAnswer: "Paris",
+    },
+    {
+      question: "Who developed the theory of relativity?",
+      options: ["Newton", "Einstein", "Galileo", "Tesla"],
+      correctAnswer: "Einstein",
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Earth", "Mars", "Jupiter", "Saturn"],
+      correctAnswer: "Jupiter",
+    },
+    {
+      question: "What is the freezing point of water?",
+      options: ["0°C", "32°F", "100°F", "100°C"],
+      correctAnswer: "0°C",
+    },
+  ];
+  
+  export default questions;
+  
